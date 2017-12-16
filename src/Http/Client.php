03a52695169258a9290a5d2fb5ec57dffb68d0b9
Jhonatan Laguna <hola@jhonatanlaguna.com>
2017-12-16 12:00:36 -0700

    Client and Response

--- src/Http/Client.php
+++ src/Http/Client.php
@@ -9,21 +9,40 @@ use Exceptions\InvalidArgument;
 
 class Client
 {
-
-    private $client = null;
-
     const BASE_URL = "https://www.facturapi.io/v1";
 
+    /** @var \GuzzleHttp\Client */
+    private $client = null;
+
+    /** @var string */
     public $api_key;
+
+    /**
+     * Guzzle allows options into its request method. Prepare for some defaults
+     * @var array
+     */
+    protected $clientOptions = [];
+    /**
+     * if set to false, no Response object is created, but the one from Guzzle is directly returned
+     * comes in handy own error handling
+     *
+     * @var bool
+     */
+    protected $wrapResponse = true;
 
     /**
      * Make it, baby.
      *
-     * @param  string $key Facturapi valid api key
-     * @param  GuzzleClient $client The Http Client (Defaults to Guzzle)
+     * @param string $key Facturapi valid api key
+     * @param GuzzleClient $client The Http Client (Defaults to Guzzle)
+     * @param array $clientOptions options to be passed to Guzzle upon each request
+     * @param bool $wrapResponse wrap request response in own Response object
      */
-    public function __construct($key, $client = null)
+    public function __construct($key, $client = null, $clientOptions = [], $wrapResponse = true)
     {
+
+        $this->clientOptions = $clientOptions;
+        $this->wrapResponse = $wrapResponse;
 
         $this->api_key = isset($key) ? $key : getenv("FACTURAPI_KEY");
         if (empty($this->key)) {
@@ -41,22 +60,38 @@ class Client
      * @param  string $endpoint The Facturapi API endpoint
      * @param  array $options An array of options to send with the request
      * @param  string $query_string A query string to send with the request
-     * @param  boolean $requires_auth Whether or not this endpoint requires authentication
      * @return Response|ResponseInterface
      * @throws BadRequest
      */
-    public function request($method, $endpoint, $options = [], $query_string = null, $requires_auth = true)
+    public function request($method, $endpoint, $options = [], $query_string = null)
     {
         $url = $this->generateUrl($endpoint, $query_string, $requires_auth);
-        $options["headers"]["Authorization"] = "Basic " . $this->key;
+        $options = array_merge($this->clientOptions, $options);
+        $options["headers"]["Authorization"] = "Basic " . base64_encode($this->key.":");
 
         try {
+            if ($this->wrapResponse === false) {
+                return $this->client->request($method, $url, $options);
+            }
             return new Response($this->client->request($method, $url, $options));
         } catch (\GuzzleHttp\Exception\BadResponseException $e) {
             throw new BadRequest(\GuzzleHttp\Psr7\str($e->getResponse()), $e->getCode(), $e);
         } catch (\Exception $e) {
             throw new BadRequest($e->getMessage(), $e->getCode(), $e);
         }
+    }
+
+
+    /**
+     * Generate the full endpoint url, including query string.
+     *
+     * @param  string  $endpoint      The HubSpot API endpoint.
+     * @param  string  $query_string  The query string to send to the endpoint.
+     * @return string
+     */
+    protected function generateUrl($endpoint, $query_string = null)
+    {
+        return BASE_URL.$endpoint."?".$query_string;
     }
 
 }
--- src/Http/Response.php
+++ src/Http/Response.php
@@ -1,5 +1,145 @@
-<?php 
+<?php
 
-class Response {
-    //
+use ArrayAccess;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\StreamInterface;
+
+class Response implements ResponseInterface, ArrayAccess
+{
+    /**
+     * @var \Psr\Http\Message\ResponseInterface
+     */
+    protected $response;
+    
+    /**
+     * @var mixed
+     */
+    public $data;
+
+
+    /**
+     * @param \Psr\Http\Message\ResponseInterface $response
+     */
+    public function __construct(ResponseInterface $response)
+    {
+        $this->response = $response;
+        $this->data = $this->getDataFromResponse($response);
+    }
+
+
+    /**
+     * Get the api data from the response as usual.
+     *
+     * @param  string  $name
+     * @return mixed
+     */
+    public function __get($name)
+    {
+        return $this->data->$name;
+    }
+
+
+    /**
+     * @param  ResponseInterface $response
+     * @return mixed
+     */
+    private function getDataFromResponse(ResponseInterface $response)
+    {
+        $contents = $response->getBody()->getContents();
+        return $contents ? json_decode($contents) : null;
+    }
+
+
+    /**
+     * Get the underlying data.
+     *
+     * @return mixed
+     */
+    public function getData()
+    {
+        return $this->data;
+    }
+
+
+    /**
+     * Return an array of the data.
+     *
+     * @return array
+     */
+    public function toArray()
+    {
+        return json_decode(json_encode($this->data), true);
+    }
+
+
+    /**
+     * Retrieves all message header values.
+     * 
+     * While header names are not case-sensitive, getHeaders() will preserve the
+     * exact case in which headers were originally specified.
+     *
+     * @return array Returns an associative array of the message's headers. Each
+     *     key MUST be a header name, and each value MUST be an array of strings
+     *     for that header.
+     */
+    public function getHeaders()
+    {
+        return $this->response->getHeaders();
+    }
+
+
+    /**
+     * Checks if a header exists by the given case-insensitive name.
+     *
+     * @param string $name Case-insensitive header field name.
+     * @return bool Returns true if any header names match the given header
+     *     name using a case-insensitive string comparison. Returns false if
+     *     no matching header name is found in the message.
+     */
+    public function hasHeader($name)
+    {
+        return $this->response->hasHeader($name);
+    }
+
+
+    /**
+     * Retrieves a message header value by the given case-insensitive name.
+     *
+     * This method returns an array of all the header values of the given
+     * case-insensitive header name.
+     *
+     * If the header does not appear in the message, this method MUST return an
+     * empty array.
+     *
+     * @param string $name Case-insensitive header field name.
+     * @return string[] An array of string values as provided for the given
+     *    header. If the header does not appear in the message, this method MUST
+     *    return an empty array.
+     */
+    public function getHeader($name)
+    {
+        return $this->response->getHeader($name);
+    }
+
+
+    /**
+     * Gets the body of the message.
+     *
+     * @return StreamInterface Returns the body as a stream.
+     */
+    public function getBody()
+    {
+        return $this->response->getBody();
+    }
+
+
+    /**
+     * Gets the response status code.
+     *
+     * @return int Status code.
+     */
+    public function getStatusCode()
+    {
+        return $this->response->getStatusCode();
+    }
 }
