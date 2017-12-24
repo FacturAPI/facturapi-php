26475cb05d2659aefa2da4fce65e76ecc2b8e7fd
Jhonatan Laguna <hola@jhonatanlaguna.com>
2017-12-24 12:15:02 -0700

    Implemented download of invoices (zip,pdf and xml) and send by email

--- src/Resources/Invoices.php
+++ src/Resources/Invoices.php
@@ -1,8 +1,8 @@
 <?php
 
 namespace Facturapi\Resources;
-require_once './Http/BaseClient.php';
-require_once './Exceptions/Facturapi_Exception.php';
+require_once 'src//Http/BaseClient.php';
+require_once 'src//Exceptions/Facturapi_Exception.php';
 
 use Facturapi\Http\BaseClient;
 use Facturapi\Exceptions\Facturapi_Exception;
@@ -82,4 +82,71 @@ class Invoices extends BaseClient {
 		}
 	}
 
+	/**
+	 * Sends the invoice to the customer's email
+	 *
+	 * @param id : Unique ID for Invoice
+	 *
+	 * @return JSON object for requested Invoice
+	 *
+	 * @throws Facturapi_Exception
+	 **/
+	public function send_by_email( $id ) {
+		try {
+			return json_decode( $this->execute_post_request( $this->get_request_url( $id ) . "/email", null ) );
+		} catch ( Facturapi_Exception $e ) {
+			throw new Facturapi_Exception( 'Unable to send Invoice: ' . $e );
+		}
+	}
+
+	/**
+	 * Downloads the specified invoice in a ZIP package containing both PDF and XML files
+	 *
+	 * @param id : Unique ID for Invoice
+	 *
+	 * @return ZIP file in a stream
+	 *
+	 * @throws Facturapi_Exception
+	 **/
+	public function download_zip( $id ) {
+		try {
+			return $this->execute_get_request( $this->get_request_url( $id ) . "/zip" );
+		} catch ( Facturapi_Exception $e ) {
+			throw new Facturapi_Exception( 'Unable to download ZIP file: ' . $e );
+		}
+	}
+
+	/**
+	 * Downloads the specified invoice in a PDF file
+	 *
+	 * @param id : Unique ID for Invoice
+	 *
+	 * @return PDF file in a stream
+	 *
+	 * @throws Facturapi_Exception
+	 **/
+	public function download_pdf( $id ) {
+		try {
+			return $this->execute_get_request( $this->get_request_url( $id ) . "/pdf" );
+		} catch ( Facturapi_Exception $e ) {
+			throw new Facturapi_Exception( 'Unable to download PDF file: ' . $e );
+		}
+	}
+
+	/**
+	 * Downloads the specified invoice in a XML file
+	 *
+	 * @param id : Unique ID for Invoice
+	 *
+	 * @return XML file in a stream
+	 *
+	 * @throws Facturapi_Exception
+	 **/
+	public function download_xml( $id ) {
+		try {
+			return $this->execute_get_request( $this->get_request_url( $id ) . "/xml" );
+		} catch ( Facturapi_Exception $e ) {
+			throw new Facturapi_Exception( 'Unable to download XML file: ' . $e );
+		}
+	}
 }
