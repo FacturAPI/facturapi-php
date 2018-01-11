67e13d85a6ebfeec247bed6d1bbacd168b1541e4
Jhonatan Laguna <hola@jhonatanlaguna.com>
2018-01-10 22:29:43 -0700

    Removed unnecessary instances

--- src/Facturapi.php
+++ src/Facturapi.php
@@ -10,22 +10,16 @@ require_once 'Constants/TaxType.php';
 use Facturapi\Resources\Customers;
 use Facturapi\Resources\Products;
 use Facturapi\Resources\Invoices;
-use Facturapi\Constants\PaymentForm;
-use Facturapi\Constants\TaxType;
 
 class Facturapi {
 
 	public $Customers;
 	public $Products;
 	public $Invoices;
-	public $PaymentForm;
-	public $TaxType;
 
 	public function __construct( $api_key ) {
-		$this->Customers = new Customers( $api_key );
-		$this->Products  = new Products( $api_key );
-		$this->Invoices  = new Invoices( $api_key );
-		$this->PaymentForm = new PaymentForm();
-		$this->TaxType = new TaxType();
+		$this->Customers   = new Customers( $api_key );
+		$this->Products    = new Products( $api_key );
+		$this->Invoices    = new Invoices( $api_key );
 	}
 }
