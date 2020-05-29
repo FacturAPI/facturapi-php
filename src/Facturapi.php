bbfb903565e7443c4dc3d25aeca73512aeb389cf
javorosas <javier.rosasb@gmail.com>
2020-05-29 21:14:30 +0200

    add support for catalogs API

--- src/Facturapi.php
+++ src/Facturapi.php
@@ -6,12 +6,14 @@ require_once 'Resources/Organizations.php';
 require_once 'Resources/Products.php';
 require_once 'Resources/Invoices.php';
 require_once 'Resources/Receipts.php';
+require_once 'Resources/Catalogs.php';
 
 use Facturapi\Resources\Customers;
 use Facturapi\Resources\Organizations;
 use Facturapi\Resources\Products;
 use Facturapi\Resources\Invoices;
 use Facturapi\Resources\Receipts;
+use Facturapi\Resources\Catalogs;
 
 class Facturapi {
 
@@ -20,6 +22,7 @@ class Facturapi {
 	public $Products;
 	public $Invoices;
 	public $Receipts;
+	public $Catalogs;
 
 	public function __construct( $api_key ) {
 		$this->Customers     = new Customers( $api_key );
@@ -27,5 +30,6 @@ class Facturapi {
 		$this->Products      = new Products( $api_key );
 		$this->Invoices      = new Invoices( $api_key );
 		$this->Receipts      = new Receipts( $api_key );
+		$this->Catalogs      = new Catalogs( $api_key );
 	}
 }
---
+++ src/Resources/Catalogs.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Facturapi\Resources;
+
+use Facturapi\Http\BaseClient;
+use Facturapi\Exceptions\Facturapi_Exception;
+
+class Catalogs extends BaseClient {
+	protected $ENDPOINT = 'catalogs';
+	protected $API_VERSION = 'v1';
+
+
+	/**
+	 * Search a product key in SAT's catalog
+	 *
+   * @param $params Search parameters
+   *
+   * @return JSON objects for all Receipts
+	 *
+	 * @throws Facturapi_Exception
+	 **/
+	public function searchProducts( $params = null ) {
+		try {
+			return json_decode(
+        $this->execute_get_request(
+          $this->get_request_url( "products" ) . $this->array_to_params($params)
+        )
+      );
+		} catch ( Facturapi_Exception $e ) {
+			throw new Facturapi_Exception( 'Unable to get receipts: ' . $e );
+		}
+	}
+
+	/**
+	 * Search a unit key in SAT's catalog
+	 *
+   * @param $params Search parameters
+   *
+   * @return JSON objects for all Receipts
+	 *
+	 * @throws Facturapi_Exception
+	 **/
+	public function searchUnits( $params = null ) {
+		try {
+			return json_decode(
+        $this->execute_get_request(
+          $this->get_request_url( "products" ) . $this->array_to_params($params)
+        )
+      );
+		} catch ( Facturapi_Exception $e ) {
+			throw new Facturapi_Exception( 'Unable to get receipts: ' . $e );
+		}
+	}
+
+}
